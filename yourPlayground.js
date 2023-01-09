03104d0bc1c9b8265c77070fdfdf571caebb6caf
ejsadiarin <ejsadiarin@gmail.com>
2023-01-09 18:00:15 +0800

    added Array Methods with proper  documentation

--- yourPlayground.js
+++ yourPlayground.js
@@ -31,8 +31,48 @@ console.log(floorRandom());
 
 // Documentation Best Practices:
 
-/* In this format
+/* In this format (for functions)
 * @param: {number} num1 first number to add
 * @param: {number} num2 second number to add
 * @return {number} returns result sum of adding num1 and num2
 */
+
+// Array
+let letters = ['a', 'b', 'c', 'd', 'e'];
+console.log(letters);
+console.log(letters[0]);
+
+// Array methods
+
+/* **********************************
+1: push --> adds new value to the end of the array
+*/
+
+letters.push("f"); 
+console.log(letters);
+
+/* **********************************
+2: slice --> gets value from array
+*/
+
+console.log(letters.slice(0, 3)); 
+/*
+* @param {indexStart} - index of the value in the array to start from
+* @param {indexEnd} - x (index of value you want to get) < indexEnd
+*
+* Basically: indexStart < x < indexEnd
+*/
+
+// Get d, e, f from letters array:
+console.log(letters.slice(3, 6));
+
+/* **********************************
+3: indexOf --> gets index of the value selected from the specific array
+*/
+console.log(letters.indexOf('e'));
+
+/* **********************************
+4: length --> gets the total index length of an array
+*/
+console.log(letters.length);
+

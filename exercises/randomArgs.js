a351f0fa98cdfccac50cde28bb6c7558fda1e2f9
ejsadiarin <ejsadiarin@gmail.com>
2023-01-13 21:52:44 +0800

    added other solution if data (number) is coming from an API

--- exercises/randomArgs.js
+++ exercises/randomArgs.js
@@ -1,5 +1,5 @@
 // Get a random number and set minimum value to 1 and max to 734
-
+// Own solution
 const minMax = (number) => {
     let result = 0;
     // reroll
@@ -13,3 +13,12 @@ const minMax = (number) => {
 }
 // @argument {number} 734 - defines the maximum value
 console.log(minMax(Math.floor(Math.random() * 735)));
+
+// =========================================================================== //
+// Other solution (superhero)
+const otherMinMax = () => {
+    const numberOfHeroes = 731;
+    return Math.floor(Math.random() * numberOfHeroes) + 1;
+}
+
+console.log(otherMinMax); // doesn't print any because the data is from an API (see /api/superHero/script.js)
